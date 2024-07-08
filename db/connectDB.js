21df0cafcb37c2e4edcd3260974d17ed8b6ca762
Rishi Goyal <rishig516@gmail.com>
2024-07-08 14:43:28 +0530

    new update

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = () => {
   return mongoose
-    .connect(process.env.DB_URL_LOCAL)
+    .connect(process.env.live_url)
     .then((data) => {
       console.log(`Mongodb connect with server : ${data.connection.host}`);
     })
